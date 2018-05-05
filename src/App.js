86a4e6e7a9a65b44256d8eb960d72d203e2e374e
Shweta Khandelwal <shweta.khandelwal2012@gmail.com>
2018-05-04 21:58:23 -0500

    initial commit
    copied everything from previous episode

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+let defaultStyle={
+  width:"15%",
+   display:'inline-block'
+}
+class Aggregate extends Component{
+  render(){
+    return(
+      <div style= {{...defaultStyle}}>
+      <h2>Number text</h2>
+      </div>
+    );
+  }
+}
+class Filter extends Component{
+  render(){
+    return(
+<div>
+  <img/>
+  <input type="text"/>
+  
+  </div>
+    );
+  }
+}
+class Playlist extends Component{
+  render(){
+    return(
+      <div style={defaultStyle}>
+<img/>
+<h3> Playlist Name </h3>
+<ul><li>Song1</li><li>Song2</li><li>Song3</li><li>Song4</li></ul>
+</div>
+    );
+  }
+}
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <h1>Title</h1>
+        <Aggregate></Aggregate>
+        <Aggregate></Aggregate>
+        <Filter/>
+        <Playlist/>
+        <Playlist/>
+        <Playlist/>
+        <Playlist/>
+      </div>
+    );
+  }
+}
+
+export default App;
